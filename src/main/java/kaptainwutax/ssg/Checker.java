d53625d068557ed583b8e7fc4318891c585a547b
Neil <h66r10@gmail.com>
2020-08-05 17:27:09 +0200

    checker 2

--- src/main/java/kaptainwutax/ssg/Checker.java
+++ src/main/java/kaptainwutax/ssg/Checker.java
@@ -10,9 +10,7 @@ import kaptainwutax.seedutils.mc.pos.CPos;
 import kaptainwutax.seedutils.util.BlockBox;
 
 import java.io.*;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -22,7 +20,7 @@ public class Checker {
 
     public static void main(String[] args) throws IOException {
         MCVersion version = MCVersion.v1_16;
-        File file = new File("valid.txt");
+        File file = new File("nonpresent.txt");
         if (!file.exists()) {
 
             if (!file.createNewFile()) {
@@ -33,57 +31,36 @@ public class Checker {
         System.out.println(file.getAbsolutePath());
         FileWriter fileWriter = new FileWriter(file);
         long startTime = System.nanoTime();
-        InputStream in = WorldSeedGenerator.class.getResourceAsStream("/input12eyes.txt");
+        InputStream in = WorldSeedGenerator.class.getResourceAsStream("/validation.txt");
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        List<String> eyes = reader.lines().collect(Collectors.toList());
-        for (String s : eyes) {
-            String[] line = s.trim().split(Pattern.quote(" "));
-            long structureSeed = Long.parseLong(line[0]);
-            CPos _12eyeChunk = new CPos(Integer.parseInt(line[1]), Integer.parseInt(line[2]));
-            CPos startChunk = new CPos(Integer.parseInt(line[3]), Integer.parseInt(line[4]));
-            long rngSeed = RING_SKIP.nextSeed(structureSeed ^ LCG.JAVA.multiplier);
-
-
-            Collection<CPos> goodStarts = getGoodStarts(structureSeed, _12eyeChunk, startChunk, version);
-            if (goodStarts.isEmpty()) {
-                continue;
+        Set<Long> eyes = reader.lines().map(el->{
+            String[] line=el.trim().split(Pattern.quote(" "));
+            return Long.parseLong(line[0]); // 258366710815118 77 -145 68 -153
+        }).collect(Collectors.toSet());
+        Set<Long> track=new HashSet<>(eyes);
+        InputStream in2 = WorldSeedGenerator.class.getResourceAsStream("/final.txt");
+        BufferedReader reader2 = new BufferedReader(new InputStreamReader(in));
+        List<Long> full = reader.lines().map(el->{
+            String[] line=el.trim().split(Pattern.quote(" "));
+            return Long.parseLong(line[2]); // 3:World seed 60933461081917201 /tp 1751 ~ -1768
+        }).collect(Collectors.toList());
+        for (Long worldseed : full) {
+            long struct=worldseed&0xFFFF_FFFF_FFFFL;
+            if (eyes.contains(struct)){
+                track.remove(struct);
+            }else{
+                System.out.println("WARNING the worldseed was not even a valid struct seed "+worldseed);
             }
-            fileWriter.write(s + "\n");
-            printTime(startTime);
         }
+        for (Long aLong : track) {
+            System.out.println("MISSING "+aLong);
+        }
+
     }
 
     private static void printTime(long start) {
         System.out.println((System.nanoTime() - start) / 1.0e9 + "s");
     }
 
-    private static Collection<CPos> getGoodStarts(long structureSeed, CPos eyeChunk, CPos startChunk, MCVersion version) {
-        Collection<CPos> goodStarts = new HashSet<>();
 
-        for (int ox = -13; ox <= 13; ox++) {
-            for (int oz = -13; oz <= 13; oz++) {
-                StrongholdGenerator generator = new StrongholdGenerator(version);
-                CPos testStart = new CPos(startChunk.getX() + ox, startChunk.getZ() + oz);
-
-                generator.generate(structureSeed, testStart.getX(), testStart.getZ(), piece -> {
-                    if (!(piece instanceof PortalRoom)) return true;
-
-                    BlockBox chunkBB = new BlockBox(eyeChunk.getX() << 4, 0, eyeChunk.getZ() << 4, (eyeChunk.getX() << 4) + 15, 255, (eyeChunk.getZ() << 4) + 15);
-
-                    BlockBox portalBB = PortalFrame.getPortalBB((PortalRoom) piece);
-                    if (!portalBB.intersects(chunkBB)) return false;
-
-                    for (Stronghold.Piece piece1 : generator.pieceList) {
-                        if (piece1 == piece) continue;
-                        if (piece1.getBoundingBox().intersects(chunkBB)) return false;
-                    }
-
-                    goodStarts.add(testStart);
-                    return false;
-                });
-            }
-        }
-
-        return goodStarts;
-    }
 }
