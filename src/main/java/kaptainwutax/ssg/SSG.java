1190ad1fea6fce6e7e416934f01781adbb6adc68
Neil <h66r10@gmail.com>
2020-08-06 00:08:28 +0200

    comp

--- src/main/java/kaptainwutax/ssg/SSG.java
+++ src/main/java/kaptainwutax/ssg/SSG.java
@@ -4,61 +4,42 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class SSG {
     public static class Data {
-        private int posX;
-        private int poxZ;
-        private long seed;
-        private double distance;
+        private final int posX;
+        private final int posZ;
+        private final long seed;
+        private final double distance;
 
         public Data(long seed, int posX, int posZ) {
             this.posX = posX;
-            this.poxZ = posZ;
+            this.posZ = posZ;
             this.seed = seed;
-            this.distance=posX*posX+posZ*posZ;
+            this.distance = posX * posX + posZ * posZ;
         }
 
         public double getDistance() {
             return distance;
         }
-
         public int getPosX() {
             return posX;
         }
-
-        public void setPosX(int posX) {
-            this.posX = posX;
+        public int getPosZ() {
+            return posZ;
         }
-
-        public int getPoxZ() {
-            return poxZ;
-        }
-
-        public void setPoxZ(int poxZ) {
-            this.poxZ = poxZ;
-        }
-
         public long getSeed() {
             return seed;
         }
 
-        public void setSeed(long seed) {
-            this.seed = seed;
-        }
-
         @Override
         public String toString() {
-            return "Data{" +
-                    "posX=" + posX +
-                    ", poxZ=" + poxZ +
-                    ", seed=" + seed +
-                    ", distance=" + distance +
-                    '}';
+            return "/tp @p " + posX + " ~ " + posZ +
+                    " , seed: " + seed +
+                    " , distance: " + distance;
         }
     }
 
@@ -74,9 +55,9 @@ public class SSG {
         for (Data seed : seeds) {
 
         }
-       // seeds.sort(Comparator.comparingDouble(Data::getDistance));
-       // for (int i = 0; i < 10000; i++) {
-       //     System.out.println(seeds.get(i));
-       // }
+        seeds.sort(Comparator.comparingDouble(Data::getDistance));
+        for (int i = 0; i < 10000; i++) {
+            System.out.println(seeds.get(i));
+        }
     }
 }
